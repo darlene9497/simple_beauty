f00617737c2473e86cf9e329b6f7ff138d74a7a8
darlene9497 <darlene.nandabi@student.moringaschool.com>
2025-05-05 21:28:56 +0300

    :sparkles: add home page with hero section, styles, and images

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Load navbar
 fetch('navbar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('navbar').innerHTML = data;
+});
+
+// Load home content
+fetch('home.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('home').innerHTML = data;
 });
